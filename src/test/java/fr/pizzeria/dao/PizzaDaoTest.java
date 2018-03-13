bfc4954a941f188272f6659731f0c02c0ae77db8
Gleamoh <ketwenty@KM-YOGA14.localdomain>
2018-03-13 17:15:56 +0100

    part 1

---
+++ src/test/java/fr/pizzeria/dao/PizzaDaoTest.java
@@ -0,0 +1,87 @@
+package fr.pizzeria.dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.DataOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Test;
+
+import fr.pizzeria.exception.SavePizzaException;
+import fr.pizzeria.model.CategoriePizza;
+import fr.pizzeria.model.Pizza;
+
+public class PizzaDaoTest {
+	
+	
+
+	private static List<Pizza> list() {
+		List<Pizza> list = new ArrayList<>();
+		list.add(new Pizza(0, null, 0));
+		list.add(new Pizza(1, null, 0));
+		list.add(new Pizza(100, null, 0));
+		list.add(new Pizza(-1, null, 0));
+		
+		list.add(new Pizza(null, null, 0, null));
+		list.add(new Pizza("DDD", "dddddd", 0.4, CategoriePizza.POISSON));
+		return list;
+	}
+
+	@Test
+	public void testGetInstance() {
+		assertTrue(PizzaDao.getInstance() instanceof PizzaDao);
+		assertTrue(PizzaDao.getInstance() != null);
+	}
+
+	@Test
+	public void testFindAllPizzas() {
+
+		assertTrue(PizzaDao.getInstance().findAllPizzas() instanceof ArrayList);
+		assertTrue(PizzaDao.getInstance().findAllPizzas() == null);
+		assertTrue(PizzaDao.getInstance().findAllPizzas().size() == 8);
+	}
+
+	@Test
+	public void testSaveNewPizzaWithValues() {
+		// int est immuable il faut utiliser un AtomicInteger à la place
+		AtomicInteger taille = new AtomicInteger(8);
+		list().stream().forEachOrdered(p -> testSaveNewPizza(p, taille.incrementAndGet()));
+	}
+
+	private void testSaveNewPizza(Pizza p, int taille) {
+		try {
+
+			PizzaDao.getInstance().saveNewPizza(p);
+			assertEquals(taille, PizzaDao.getInstance().findAllPizzas().size());
+
+		} catch (SavePizzaException e) {
+			fail(e.getMessage());
+		}
+
+	}
+
+	@Test
+	public void testUpdatePizza() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeletePizza() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindPizzaByCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPizzaExists() {
+		fail("Not yet implemented");
+	}
+
+}
